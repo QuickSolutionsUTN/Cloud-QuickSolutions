dc1bd34aa27fad721b1b2b40ddf5512ae355940c
Rodrigo C <rodrigocapre@gmail.com>
2024-10-16 11:52:35 -0300

    se continua trabajando con el back

---
+++ backend/README.txt
@@ -0,0 +1,5 @@
+Para correr por primera vez, si tira error:
+
+Desde Solucion--> administrar de paquetes NuGet --> Agregar:
+
+	EntityFrameworkCore
---
+++ backend/WebApi_Proyecto/DALCodeFirst/WebAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using DALCodeFirst.Modelos;
+using Microsoft.EntityFrameworkCore; //DbContext
+
+//La BD creada se llama:
+namespace DALCodeFirst;
+
+//contexto para la base de datos
+public partial class WebAPIContext : DbContext
+{
+    public WebAPIContext()
+    {
+    }
+    public WebAPIContext(DbContextOptions<WebAPIContext> options) : base(options) { } //constructor
+
+    //dbset para la tabla de productos y categorias
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Persist Security Info=True;Password=tallersoft600;Username=postgres;Database=S31-Productos;Port=14998;Host=colosal.duckdns.org");
+}
