491c36da8a684a0249aadc0958d10523a2288a50
damiialvite <dalvite@alu.frlp.utn.edu.ar>
2024-10-17 13:00:41 -0300

    fin

---
+++ frontend/proyect-front/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ServiceCard from '../components/ServiceCard';
+import '../styles/home.css'; 
+import {userNavigate} from 'react-router-dom';
+//import Img1 from '../assets/images/equipos.png'; // Importa la imagen
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Bienvenido a la Página Principal</h2>
+      {/* <ServiceCard title="Equipos" description="Descripción del servicio 1" imgSrc={''}/> */}
+    </section>
+  );
+};
+
+export default Home;
