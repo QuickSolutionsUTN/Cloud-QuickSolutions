e64c7a62f7ea08d93da9e5bba61174cff7c75b76
AlviteD <dalvite@alu.frlp.utn.edu.ar>
2025-11-22 13:16:31 -0300

    Modificacion en la tabla de domicilio

--- backend/quicksolutions/serializers.py
+++ backend/quicksolutions/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from .models import Perfiles, Categoria, Producto, SolicitudServicio
+from .models import Perfiles, Categoria, Producto, SolicitudServicio, Domicilio
     
 class PerfilesSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='id.email', read_only=True)
+    domicilio = serializers.SerializerMethodField()
     class Meta:
         model = Perfiles
         fields = '__all__'
+    def get_domicilio(self, obj):
+        usuario = obj.id 
+        domicilio_obj = usuario.domicilio_set.first()
+        if domicilio_obj:
+            return DomicilioSerializer(domicilio_obj).data
+        return None
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +28,10 @@ class SolicitudServicioSerializer(serializers.ModelSerializer):
     class Meta:
         model = SolicitudServicio
         fields = '__all__' # Incluye todos los campos del modelo
+
+class DomicilioSerializer(serializers.ModelSerializer):
+    ciudad = serializers.CharField(source='id_localidad.nombre', read_only=True)
+    provincia = serializers.CharField(source='id_localidad.id_provincia.nombre', read_only=True)
+    class Meta:
+        model = Domicilio
+        fields = ['calle', 'numero', 'departamento', 'codigo_postal', 'ciudad', 'provincia', 'piso']
