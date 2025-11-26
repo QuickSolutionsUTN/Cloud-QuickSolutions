5b536ddf888f8011a33578e10f51973e5524f0bd
AlviteD <dalvite@alu.frlp.utn.edu.ar>
2025-11-26 10:07:10 -0300

    Cambio en el nombre de las variables y eliminacion de envio (ya esta el domicilio)

--- backend/quicksolutions/serializers.py
+++ backend/quicksolutions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Perfiles, Categoria, Producto, SolicitudServicio, Envio, ChecklistMantenimiento, TipoMantenimiento, Domicilio, Provincia, Localidad
+from .models import Perfiles, Categoria, Producto, SolicitudServicio, ChecklistMantenimiento, TipoMantenimiento, Domicilio, Provincia, Localidad
     
 class PerfilesSerializer(serializers.ModelSerializer):
     email = serializers.CharField(source='id.email', read_only=True)
@@ -40,42 +40,42 @@ class CrearSolicitudSerializer(serializers.Serializer):
     userEmail = serializers.EmailField()
     descripcion = serializers.CharField(required=False, allow_blank=True)
     idTipoServicio = serializers.IntegerField()
-    idTipoProducto = serializers.IntegerField()
+    idProducto = serializers.IntegerField()
     idTipoMantenimiento = serializers.IntegerField(required=False, allow_null=True)
     conLogistica = serializers.BooleanField()
-    envio = EnvioSerializer(required=False, allow_null=True)
+#    envio = EnvioSerializer(required=False, allow_null=True)
 
 class SolicitudDetailSerializer(serializers.ModelSerializer):
     emailSolicitante = serializers.CharField(source='id_solicitante.email', read_only=True)
     tipoServicio = serializers.CharField(source='id_tipo_servicio.descripcion', read_only=True)
     categoria = serializers.CharField(source='id_producto.id_categoria.descripcion', read_only=True)
-    tipoDeProducto = serializers.CharField(source='id_producto.descripcion', read_only=True)
+    Producto = serializers.CharField(source='id_producto.descripcion', read_only=True)
     estado = serializers.CharField(source='id_solicitud_servicio_estado.descripcion', read_only=True)
     fechaGeneracion = serializers.DateTimeField(source='fecha_generacion', read_only=True)
     fechaEstimada = serializers.DateTimeField(source='fecha_estimada', read_only=True)
     fechaFinalizada = serializers.DateTimeField(source='fecha_finalizada', read_only=True)
     diagnosticoTecnico = serializers.CharField(source='diagnostico_tecnico', read_only=True)
     
-    envio = serializers.SerializerMethodField()
+#    envio = serializers.SerializerMethodField()
     mantenimiento = serializers.SerializerMethodField()
 
     class Meta:
         model = SolicitudServicio
         fields = [
-            'id', 'emailSolicitante', 'tipoServicio', 'categoria', 'tipoDeProducto', 
+            'id', 'emailSolicitante', 'tipoServicio', 'categoria', 'Producto', 
             'descripcion', 'estado', 'fechaGeneracion', 'monto', 'fechaEstimada', 
             'fechaFinalizada', 'diagnosticoTecnico', 'con_logistica', 'envio', 'mantenimiento'
         ]
 
-    def get_envio(self, obj):
-        if hasattr(obj, 'envio'):
-            return {
-                'nroSeguimiento': obj.envio.nro_seguimiento,
-                'calle': obj.envio.calle,
-                'numero': obj.envio.numero,
-                'localidad': obj.envio.id_localidad.nombre
-            }
-        return None
+#   def get_envio(self, obj):
+#        if hasattr(obj, 'envio'):
+#            return {
+#                'nroSeguimiento': obj.envio.nro_seguimiento,
+#                'calle': obj.envio.calle,
+#                'numero': obj.envio.numero,
+#                'localidad': obj.envio.id_localidad.nombre
+#            }
+#        return None
 
     def get_mantenimiento(self, obj):
         if obj.id_tipo_mantenimiento:
@@ -92,12 +92,12 @@ class SolicitudDetailSerializer(serializers.ModelSerializer):
 
 class TipoMantenimientoSerializer(serializers.ModelSerializer):
     # Renombramos el campo para que coincida con el frontend
-    idTipoProducto = serializers.PrimaryKeyRelatedField(source='id_producto', read_only=True)
+    idProducto = serializers.PrimaryKeyRelatedField(source='id_producto', read_only=True)
 
     class Meta:
         model = TipoMantenimiento
         # Excluimos 'id_producto' original para no enviar duplicados
-        fields = ['id', 'nombre', 'descripcion', 'idTipoProducto']
+        fields = ['id', 'nombre', 'descripcion', 'idProducto']
 class ProvinciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provincia
